90bfdcbfbb5b1a9bc7f50e93b7fe488fb7fb1af2
Matteo Pipitone <matteo.pipitone@gmail.com>
2020-12-22 12:48:24 +0100

    first commit

---
+++ src/main/java/it/pipitone/matteo/elephantcarpaccio2/ElephantCarpaccio2Application.java
@@ -0,0 +1,13 @@
+package it.pipitone.matteo.elephantcarpaccio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElephantCarpaccio2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElephantCarpaccio2Application.class, args);
+    }
+
+}
---
+++ src/test/java/it/pipitone/matteo/elephantcarpaccio2/ElephantCarpaccio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.pipitone.matteo.elephantcarpaccio2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElephantCarpaccio2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
